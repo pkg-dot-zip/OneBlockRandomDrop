8f716bebe37e553ba10ae526a9c709a4967919d8
ZimonIsHim <dutchsimon9@gmail.com>
2021-10-09 15:22:52 +0200

    Placed debug stuff in comments

--- src/main/java/com/zimonishim/oneblockrandomdrop/Oneblockrandomdrop.java
+++ src/main/java/com/zimonishim/oneblockrandomdrop/Oneblockrandomdrop.java
@@ -2,19 +2,14 @@ package com.zimonishim.oneblockrandomdrop;
 
 import com.zimonishim.oneblockrandomdrop.config.ChanceConfigContainer;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.block.Block;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.text.LiteralText;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import org.lwjgl.glfw.GLFW;
 
 public class Oneblockrandomdrop implements ModInitializer {
 
@@ -41,23 +36,23 @@ public class Oneblockrandomdrop implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier("obrd", "randomdrop_block"), new BlockItem(RANDOMDROP_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
 
         //Debug part.
-        if (allowDebugging){
-            printItemMapKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                    "key.obrd.printMap", // The translation key of the keybinding's name
-                    InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
-                    GLFW.GLFW_KEY_COMMA, // The keycode of the key
-                    "debug.obrd.main" // The translation key of the keybinding's category.
-            ));
-
-            ClientTickEvents.END_CLIENT_TICK.register(client -> {
-                while (printItemMapKeyBinding.wasPressed()) {
-                    client.player.sendMessage(new LiteralText("Key 1 was pressed!"), false);
-                    if (!debugDone){
-                        debugDone = true;
-                        RandomDropBlock.printItemMap();
-                    }
-                }
-            });
-        }
+//        if (allowDebugging){
+//            printItemMapKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+//                    "key.obrd.printMap", // The translation key of the keybinding's name
+//                    InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+//                    GLFW.GLFW_KEY_COMMA, // The keycode of the key
+//                    "debug.obrd.main" // The translation key of the keybinding's category.
+//            ));
+//
+//            ClientTickEvents.END_CLIENT_TICK.register(client -> {
+//                while (printItemMapKeyBinding.wasPressed()) {
+//                    client.player.sendMessage(new LiteralText("Key 1 was pressed!"), false);
+//                    if (!debugDone){
+//                        debugDone = true;
+//                        RandomDropBlock.printItemMap();
+//                    }
+//                }
+//            });
+//        }
     }
 }
